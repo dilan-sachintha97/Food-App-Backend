7e7566b353ecc34a89971a645d889a807c6ed291
dilan-schintha97 <dil97an@gmail.com>
2023-09-22 15:38:22 +0530

    created userSchema

--- db.js
+++ db.js
@@ -1,13 +1,30 @@
 const mongoose = require('mongoose');
 
-const mongoDBConnected = ()=>{
-    mongoose.connect('mongodb://127.0.0.1:27017/food_app',{useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
-        console.log("connected to mongoDB");
-    
-    }).catch((err)=>{
-        console.error('Error connecting to MongoDB:', err);
-    });
+const mongoDBConnected = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/food_app', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log("Connected to MongoDB");
+
+        const collection_data = mongoose.connection.db.collection("food_item");
+
+        collection_data.find({}).toArray((err, data) => {
+            if (err) {
+                console.error("Error retrieving data from 'food_item' collection:", err);
+            } else {
+                console.log(data);
+            }
+            
+            // Close the MongoDB connection when done.
+            mongoose.connection.close();
+        });
+
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+    }
 }
 
 module.exports = mongoDBConnected;
-
